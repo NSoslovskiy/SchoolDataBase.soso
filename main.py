0c35448195df7abc53ad6d69cc03e82545ae885f
NSoslovskiy <nazarii.soslovskiy@gmail.com>
2026-02-09 17:35:56 +0200

    фінальна версія

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import csv
+import json
+
 with open("config.txt", "r") as config:
     min_score = int(config.read())
 
@@ -12,7 +14,7 @@ with open("students.csv", "r",encoding="utf-8") as file:
 
 retest_list=[]
 for s in students:
-    if s["score"] < min_score:
+    if s["score"] <= min_score:
         retest_list.append(s)
 
 with open("retest.csv", "w", encoding="utf-8", newline="") as file:
@@ -21,5 +23,16 @@ with open("retest.csv", "w", encoding="utf-8", newline="") as file:
     writer.writeheader()
     for s in retest_list:
         writer.writerow(s)
+
+success_list = []
+for s in students:
+    if s["score"] >= min_score:
+        success_list.append(s)
+
+for s in success_list:
+    s["passed"] = True
+
+with open("top_students.json", "w", encoding="utf-8") as json_file:
+    json.dump(success_list, json_file, indent=4, ensure_ascii=False)
 
 
